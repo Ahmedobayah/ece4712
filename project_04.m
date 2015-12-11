e1f22540ca86744245f618893ef2d7c85f822cdd
Devin Jiang <devin.jiang@temple.edu>
2015-12-11 10:13:51 -0500

    Initial

---
+++ project_04.m
@@ -0,0 +1,8 @@
+%% ECE 4712 - Project 4
+% Devin Jiang, John Snyder, Abdulmagid Dahbali
+% Various parameters are:
+% Generator: X1 = 0.12 pu, X2 = 0.12 pu, X0 = 0.06 pu
+% Transformer: X1 = 0.10 pu, X2 = 0.10 pu, X0 = 0.08 pu (same for both transformers) Transmission Line: X1 = 0.15 pu, X2 = 0.15 pu, X0 = 0.20 pu
+% Load: S = 1.0 + j0.5 pu (per phase)
+% Bus 1 Voltage: V1 = 1.0 ?0 pu
+% Fault impedance: Zf = 0.05 pu
